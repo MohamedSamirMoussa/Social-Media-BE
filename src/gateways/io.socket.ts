2c62e8079840b467342e7627fce2561a0baccd61
Mohamed Samir <mosamir5000@gmail.com>
2026-09-04 19:56:50 +0300

    fixxxxxxx

--- src/gateways/io.socket.ts
+++ src/gateways/io.socket.ts
@@ -17,7 +17,7 @@ export const emitOnlineUsers = () => {
 
 export const socketAuthentication = async (socket: Socket, next: Function) => {
   try {
-    const cookies = cookie.parseCookie(socket.handshake.headers.cookie || "");
+    const cookies = cookie.parse(socket.handshake.headers.cookie || "");
     if (!cookies) return next(new NotAuthorizedError("Unauthorized"));
     const accessToken = cookies.access_token;
     if (!accessToken) return next(new ConflictError("Access token not found"));
