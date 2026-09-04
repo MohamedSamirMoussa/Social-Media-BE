f2d61b3e945d775127a101f0c95e1fc1e4dd6fbc
Mohamed Samir <mosamir5000@gmail.com>
2026-09-04 22:02:37 +0300

    fixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

--- src/utils/multer/cloudinary.config.ts
+++ src/utils/multer/cloudinary.config.ts
@@ -1,13 +1,12 @@
-import { v2 as cloudinary } from "cloudinary"
-
+import { v2 as cloudinary } from "cloudinary";
 
 export const cloud = () => {
-   cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_NAME as string,
-    api_key:process.env.CLOUDINARY_APi_KEY as string,
-    api_secret:process.env.CLOUDINARY_APi_SECRET as string,
-    secure:true,
-   })
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME as string,
+    api_key: process.env.CLOUDINARY_API_KEY as string,
+    api_secret: process.env.CLOUDINARY_API_SECRET as string,
+    secure: true,
+  });
 
-   return cloudinary
-}
+  return cloudinary;
+};
