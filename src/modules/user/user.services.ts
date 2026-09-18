6cbb22825a59f10b67a242f9a2224bf2f634bf19
Mohamed Samir <mosamir5000@gmail.com>
2026-09-18 04:25:32 +0300

    fix compare hashed password

--- src/modules/user/user.services.ts
+++ src/modules/user/user.services.ts
@@ -122,7 +122,7 @@ class UserServices {
     if (!user) throw new BadRequestError("This account doesn't exists");
     if (!user.confirmedAt)
       throw new ConflictError("Verify your account please check your email");
-    if (user.password || !(await compareHash(password, user.password)))
+    if (!user.password || !(await compareHash(password, user.password)))
       throw new BadRequestError("Invalid credentials");
     await this.handleLogin(res, user);
     return successHandler({
