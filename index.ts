66fac5e7161df3d8fe3cbbd9a82fb4851de23a96
Mohamed Samir <mosamir5000@gmail.com>
2026-09-18 04:00:21 +0300

    check

--- index.ts
+++ index.ts
@@ -2,19 +2,10 @@ import { config } from "dotenv";
 import bootstrap from "./src/bootstrap";
 import { resolve } from "node:path";
 import express from "express";
-import { existsSync } from "node:fs";
 
-const envPath = resolve("./config/.env.development");
-
-const result = config({ path: envPath });
-
-console.log("ENV CHECK:", {
-  fileExists: existsSync(envPath),
-  fileReadError: result.error?.message ?? null,
-  DB_URL_EXISTS: Boolean(process.env.DB_URL),
-  FE_URI_EXISTS: Boolean(process.env.FE_URI),
-});
-
+if (process.env.VERCEL !== "1") {
+  config({ path: resolve("./config/.env.development") });
+}
 const app = express();
 const server = bootstrap(app);
 
